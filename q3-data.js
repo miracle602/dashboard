f3b95255009bce05ada03977a19f99c8e5e5fb8d
miracle <ynmiracle@miraclendiannao.lan>
2026-04-28 14:42:04 +0800

    feat: Q2季报(近3月/同比环比/毛利率/品类门店)

---
+++ q3-data.js
@@ -0,0 +1,18 @@
+var Q3M = {
+    summary: {
+        amt: 283726, amt_last: 484982.9, amt_prev: 147123.2,
+        list_amt: 307444, list_last: 529835.2,
+        qty: 909, qty_last: 1334, qty_prev: 448,
+        orders: 905, orders_last: 1020,
+        gross_rate: 55.0, gross_rate_last: 55.0, gross_profit: 156049.3,
+        discount: 92.3, discount_last: 91.5,
+        yoy_sale: -41.5, yoy_qty: -31.9, mom_sale: 92.8, mom_qty: 102.9,
+        vip_amt: 236110, vip_count: 5,
+        stores: 2, store_total: 124,
+        products: 13
+    },
+    months: {"202602": {"amt": 91866, "list": 91866, "qty": 289, "orders": 289}, "202603": {"amt": 105598, "list": 120886, "qty": 351, "orders": 348}, "202604": {"amt": 86262, "list": 94692, "qty": 269, "orders": 268}},
+    months_last: {"202502": {"amt": 148139.6, "list": 163646.0, "qty": 468, "orders": 342}, "202503": {"amt": 201542.6, "list": 219508.2, "qty": 500, "orders": 351}, "202504": {"amt": 135300.7, "list": 146681, "qty": 366, "orders": 327}},
+    stores: {"10": {"name": "小西门店", "orders": 904, "amt": 283428, "qty": 908}, "155": {"name": "测试店仓", "orders": 1, "amt": 298, "qty": 1}},
+    cats: {"连衣裙": {"qty": 252, "amt": 119952}, "套装": {"qty": 420, "amt": 90720}, "微喇裤": {"qty": 168, "amt": 58632}, "其他": {"qty": 41, "amt": 7128.0}, "鞋子": {"qty": 12, "amt": 1800}, "XS戒指": {"qty": 2, "amt": 1580}, "椅子": {"qty": 3, "amt": 1380}, "裤子": {"qty": 4, "amt": 934.0}, "秋季棉服": {"qty": 2, "amt": 598}, "中长A裙": {"qty": 1, "amt": 366}, "A字短裤": {"qty": 1, "amt": 270}, "长裤": {"qty": 2, "amt": 200}, "公主鞋": {"qty": 1, "amt": 149}}
+};
